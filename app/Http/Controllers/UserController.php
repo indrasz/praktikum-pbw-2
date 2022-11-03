361b5551dcb4d1d0c7cdd66361c374b7a2ab47c1
wahyuindra <wahyuindra712@gmail.com>
2022-11-03 12:05:40 +0700

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('page.page');
+        return view('user.daftarPengguna');
     }
 
     /**
